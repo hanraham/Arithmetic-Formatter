c021e79a3722a6c8bb1addd7f89a8c1162fba123
AMH <hanraham@gmail.com>
2021-03-15 15:48:53 -0400

    First round of work on problem, need to go pick up Charlotte

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,4 +1,31 @@
-def arithmetic_arranger(problems):
+def arithmetic_arranger(problems, show_answer = False):
+    # Error checking
 
+    #Can't be more then 5 problems
+    if len(problems) > 5:
+        return 'Error: Too many problems.'
+    # Operands must be addition and subtraction
+    # Numbers must be digits only
+    # Numbers can only have 4 digits
+
+    for problem in problems:
+        split_problem = problem.split()
+
+        try:
+            int(split_problem[0])
+            int(split_problem[2])
+        except:
+            return 'Error: Numbers must only contain digits.'
+        
+        if split_problem[1] != '+' and split_problem[1] != '-':
+            return "Error: Operator must be '+' or '-'."
+        
+        
+
+    
+    arranged_problems = ""
+
+    if show_answer:
+        arranged_problems = 'Show answer'
 
     return arranged_problems
